8b9d4d6d03db5a50a7afda4c5ad2b80f2e76e3d5
denisrodin <denis.for.logs@gmail.com>
2021-01-28 15:43:18 +0300

    init

---
+++ src/test/java/com/github/rodin/denis/spring/security/demo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rodin.denis.spring.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
